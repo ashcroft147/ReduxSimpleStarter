feb10d07128e6c98a56c56026057d7196f10dc83
ashcroft147 <pd050786@VDIPS.POSCO.DOM>
2017-06-01 19:47:00 +0900

    Lecture 11

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -1,18 +1,26 @@
 import { renderComponent, expect } from  '../test_helper';
-import CommentBox from '../../components/comment_box';
+import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
 
-    const component = renderComponent(CommentBox);
+    // it에 정의된 각각의 spec이 실행되기 전에 실행되는 구문
+    // kind of setup for the each spec
+    let component; // undefined
+    beforeEach(() =>{ 
+        component = renderComponent(CommentBox);
+    });
+
+    it('has the correct class', () => {
+        expect(component).to.have.class('comment-box');
+    });
 
     it('has a text area', () => {
-        // matchers
-        expect
+        expect(component.find('textarea')).to.exist;
     });
 
 
     it('has a button', () => {
-        // matchers
+        expect(component.find('button')).to.exist;
     });
 
 });
