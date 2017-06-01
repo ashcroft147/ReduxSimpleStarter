eb93ad8106f6c9aa2c039264472fe6bbec525d32
ashcroft147 <pd050786@VDIPS.POSCO.DOM>
2017-06-01 10:23:37 +0900

    Commit first

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,15 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from  '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+// Use 'describe' to group together similar tests;
+describe('App', () => {
+  
+  // Use 'it' to test a single attribute of a target
+  it('shows the correct text', () => {
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
-
-  it('renders something', () => {
-    expect(component).to.exist;
+      // create an instance of App(helper function that renders a component)
+      const component = renderComponent(App);
+      // Use 'expect' to make an 'assertion' atbout a target
+      expect(component).to.contain('React simple starter');
   });
 });
---
+++ test/components/comment_box_test.js
@@ -0,0 +1,18 @@
+import { renderComponent, expect } from  '../test_helper';
+import CommentBox from '../../components/comment_box';
+
+describe('CommentBox', () => {
+
+    const component = renderComponent(CommentBox);
+
+    it('has a text area', () => {
+        // matchers
+        expect
+    });
+
+
+    it('has a button', () => {
+        // matchers
+    });
+
+});
