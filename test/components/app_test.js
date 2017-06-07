487e526f44c85186e6d4051376af6ecdc7245968
ashcroft147 <pd050786@VDIPS.POSCO.DOM>
2017-06-07 18:07:45 +0900

    Add code until Chap.17

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -4,6 +4,20 @@ import App from '../../src/components/app';
 // Use 'describe' to group together similar tests;
 describe('App', () => {
   
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
+
+  it('shows a comment box', ()=> {
+    expect(component.find('.comment-box')).to.exist;
+  });
+
+  it('shows a comment list', () => {
+    expect(component.find('.comment-list')).to.exist;
+  });
+  /*
   // Use 'it' to test a single attribute of a target
   it('shows the correct text', () => {
 
@@ -12,4 +26,5 @@ describe('App', () => {
       // Use 'expect' to make an 'assertion' atbout a target
       expect(component).to.contain('React simple starter');
   });
+  */
 });
